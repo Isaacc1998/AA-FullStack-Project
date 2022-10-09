92b5a6570d29130cd873c6b1730df96eaa97ad4d
Isaac <isaacc98@gmail.com>
2022-10-09 19:25:08 -0400

    backend setup with user table/model

--- flashlet/db/seeds.rb
+++ flashlet/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ApplicationRecord.transaction do 
+    puts "Destroying tables..."
+    User.destroy_all
+  
+    puts "Resetting primary keys..."
+    ApplicationRecord.connection.reset_pk_sequence!('users')
+  
+    puts "Creating users..."
+    User.create!(
+      username: 'Joe', 
+      email: 'Joe@gmail.com', 
+      password: 'password'
+    )
+
+    User.create!(
+      username: 'Bob', 
+      email: 'Bob@gmail.com', 
+      password: 'bobiscool'
+    )
+  
+    puts "Done!"
+  end
