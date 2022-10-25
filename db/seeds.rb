2caa00a383518010d54beddc4fe0de7eea79c4a0
Isaac <isaacc98@gmail.com>
2022-10-25 09:14:04 -0400

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,16 +37,155 @@ ApplicationRecord.transaction do
       author_id: 1
     )
 
-    FlashcardSet.create!(
-      title: 'Chineese',
-      author_id: 1
+    Flashcard.create!(
+      front: "1 + 1 = ?",
+      back: "2",
+      set_id: 1
+    )
+
+    Flashcard.create!(
+      front: "3 * 4 = ?",
+      back: "12",
+      set_id: 1
+    )
+
+    Flashcard.create!(
+      front: "10 / 2 = ?",
+      back: "5",
+      set_id: 1
     )
 
     FlashcardSet.create!(
-      title: 'English',
+      title: 'Demo',
+      author_id: 1
+    )
+
+    Flashcard.create!(
+      front: "term",
+      back: "definition",
+      set_id: 2
+    )
+
+    FlashcardSet.create!(
+      title: 'Study Set 1',
+      author_id: 1
+    )
+
+    Flashcard.create!(
+      front: "question",
+      back: "answer",
+      set_id: 3
+    )
+
+    FlashcardSet.create!(
+      title: 'Study Set 2',
+      author_id: 1
+    )
+
+    Flashcard.create!(
+      front: "question",
+      back: "answer",
+      set_id: 4
+    )
+
+    FlashcardSet.create!(
+      title: 'Study Set 3',
       author_id: 2
     )
-    
+
+    Flashcard.create!(
+      front: "question",
+      back: "answer",
+      set_id: 5
+    )
+
+    FlashcardSet.create!(
+      title: 'Study Set 4',
+      author_id: 2
+    )
+
+    Flashcard.create!(
+      front: "question",
+      back: "answer",
+      set_id: 6
+    )
+
+    FlashcardSet.create!(
+      title: 'Study Set 5',
+      author_id: 2
+    )
+
+    Flashcard.create!(
+      front: "question",
+      back: "answer",
+      set_id: 7
+    )
+
+    FlashcardSet.create!(
+      title: 'Study Set 6',
+      author_id: 3
+    )
+  
+    Flashcard.create!(
+      front: "question",
+      back: "answer",
+      set_id: 8
+    )
+
+    FlashcardSet.create!(
+      title: 'Study Set 6',
+      author_id: 3
+    )
+
+    Flashcard.create!(
+      front: "question",
+      back: "answer",
+      set_id: 9
+    )
+  
+    FlashcardSet.create!(
+      title: 'Study Set 7',
+      author_id: 3
+    )
+
+    Flashcard.create!(
+      front: "question",
+      back: "answer",
+      set_id: 10
+    )
+  
+    FlashcardSet.create!(
+      title: 'Study Set 8',
+      author_id: 2
+    )
+
+    Flashcard.create!(
+      front: "question",
+      back: "answer",
+      set_id: 11
+    )
+
+    FlashcardSet.create!(
+      title: 'Study Set 9',
+      author_id: 2
+    )
+
+    Flashcard.create!(
+      front: "question",
+      back: "answer",
+      set_id: 12
+    )
+
+    FlashcardSet.create!(
+      title: 'Study Set 10',
+      author_id: 2
+    )
+
+    Flashcard.create!(
+      front: "question",
+      back: "answer",
+      set_id: 13
+    )
   
     puts "Done!"
   end
